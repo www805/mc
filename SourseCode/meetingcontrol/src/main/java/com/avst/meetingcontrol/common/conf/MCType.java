44697abf4effb8b778299bdbc9a53741f4059925
zhugg <emma7727@163.com>
2019-05-24 09:54:17 +0800

    pp

---
+++ SourseCode/meetingcontrol/src/main/java/com/avst/meetingcontrol/common/conf/MCType.java
@@ -0,0 +1,11 @@
+package com.avst.meetingcontrol.common.conf;
+
+/**
+ * 会议服务器的类型
+ * 每种会议服务都有一个会议服务类型mctype
+ */
+public class MCType {
+
+    public static String AVST="MC_AVST";
+
+}
---
+++ SourseCode/meetingcontrol/src/main/java/com/avst/meetingcontrol/common/conf/YWType.java
@@ -0,0 +1,10 @@
+package com.avst.meetingcontrol.common.conf;
+
+/**
+ * 上层业务服务
+ * 每种业务服务都有一个业务服务类型ywtype
+ */
+public class YWType {
+
+    public static String RECORD_TRM="RECORD_TRM_AVST";//trm会议笔录系统
+}
