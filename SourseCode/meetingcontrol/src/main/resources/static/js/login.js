b349fb855fcd718ddb00bc976d0aa7dd4c68d9be
zjl <zjl@qq.com>
2019-07-26 11:58:12 +0800

    修改外部文件

--- SourseCode/meetingcontrol/src/main/resources/static/js/login.js
+++ SourseCode/meetingcontrol/src/main/resources/static/js/login.js
@@ -11,6 +11,16 @@ function logining(){
 
 }
 
+function logout(){
+    var url=getUrl_manage().logout;
+    ajaxSubmit(url,null,callLogout);
+}
+
+function getNavList() {
+    var url=getUrl_manage().getNavList;
+    ajaxSubmitByJson(url,null,callgetNavList);
+}
+
 function callbacklogining(data){
     if(null!=data&&data.actioncode=='SUCCESS'){
         var url=getUrl_manage().main;
@@ -20,9 +30,38 @@ function callbacklogining(data){
     }
 }
 
-function logout(){
-    var url=getUrl_manage().logout;
-    ajaxSubmit(url,null,callLogout);
+function callgetNavList(data) {
+    if(null!=data&&data.actioncode=='SUCCESS'){
+
+        if (isNotEmpty(data.data.data)) {
+            var appCache = data.data;
+
+            if (isNotEmpty(appCache.data.title)) {
+                //页头
+                var Top_Title = appCache.data.title;
+                $("#topTitle").html("欢迎进入" + Top_Title);
+                $("#spanTitle").html("欢迎进入" + Top_Title);
+            }
+
+            if (isNotEmpty(appCache.data.bottom)) {
+                if (!isNotEmpty(appCache.data.bottom) || !isNotEmpty(appCache.data.bottom.name) || !isNotEmpty(appCache.data.bottom.declaration) || !isNotEmpty(appCache.data.bottom.url)) {
+                    return;
+                }
+                //页脚
+                var bottom_name = appCache.data.bottom.name;
+                var bottom_declaration = appCache.data.bottom.declaration;
+                var bottom_url = appCache.data.bottom.url;
+                var bottom_html = bottom_declaration + " <a href=\"" + bottom_url + "\">" + bottom_name + "</a>";
+                $("#bottom_mian").html(bottom_html);
+            }
+        }
+        layui.use('element', function(){
+            var element =  layui.element;
+            element.render();
+        });
+    }else{
+        layer.msg(data.message);
+    }
 }
 
 function callLogout(data){
