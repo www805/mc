a389e02cc89c02525af7f188834ea6570e5267e8
zjl <zjl@qq.com>
2019-09-12 11:59:22 +0800

    微调页面，新增登录页logo修改，页脚logo修改

--- SourseCode/meetingcontrol/src/main/resources/static/js/main.js
+++ SourseCode/meetingcontrol/src/main/resources/static/js/main.js
@@ -50,10 +50,17 @@ function callgetNavList(data) {
                     return;
                 }
                 //页脚
+                var bottom_html = "";
                 var bottom_name = appCache.data.bottom.name;
                 var bottom_declaration = appCache.data.bottom.declaration;
                 var bottom_url = appCache.data.bottom.url;
-                var bottom_html = bottom_declaration + " <a href=\"" + bottom_url + "\" target=\"_blank\">" + bottom_name + "</a>";
+
+                if (isNotEmpty(appCache.data.bottom.img.src) && appCache.data.bottom.img.src != '/') {
+                    $(".layui-footer").css("height", "50px").css("margin-top", "5px");
+                    bottom_html = " <a href=\"" + bottom_url + "\">" + "<img style='margin-top: 5px;' width='" + appCache.data.bottom.img.width + "' height='" + appCache.data.bottom.img.height + "' src='" + appCache.data.bottom.img.src + "'>" + "</a>";
+                } else {
+                    bottom_html = bottom_declaration + " <a href=\"" + bottom_url + "\" target=\"_blank\">" + bottom_name + "</a>";
+                }
                 $("#bottom_mian").html(bottom_html);
             }
         }
